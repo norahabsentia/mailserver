77b30f843dee053cd4b7afb03a558bb81a14942c
Ashish Gupta <ashish@absentiavr.com>
2017-09-15 20:20:51 +0530

    changes

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Server started!');
 
+app.post('/mail', function(req, res) {
+    var user_id = req.param('id');
+    var token = req.param('id1');
+    var geo = req.param('id2');
 
-var mailgun = require("mailgun-js");
-var api_key = 'key-a0a6f0f537f651a6afb73234d346a045';
-var DOMAIN = 'sandbox54dd8016480441aaa29353d95fdf3890.mailgun.org';
-var mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
+    var mailgun = require("mailgun-js");
+    var api_key = 'key-a0a6f0f537f651a6afb73234d346a045';
+    var DOMAIN = 'sandbox54dd8016480441aaa29353d95fdf3890.mailgun.org';
+    var mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
 
-var data = {
-    from: 'Excited User <ashishlnmiit3@gmail.com>',
-    to: 'ashish@absentiavr.com',
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!'
-};
+    var data = {
+        from: user_id,
+        to: token,
+        subject: geo,
+        text: 'Testing some Mailgun awesomness!'
+    };
 
-mailgun.messages().send(data, function(error, body) {
-    console.log(data);
-    console.log(body);
+    mailgun.messages().send(data, function(error, body) {
+        console.log(data);
+        console.log(body);
+    });
+    res.send(user_id + ' ' + token + ' ' + geo);
 });
