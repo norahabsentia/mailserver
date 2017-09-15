2c0d982989f4bb35e214535de00e4ea0ffd2c4c0
Ashish Gupta <ashish@absentiavr.com>
2017-09-15 20:10:54 +0530

    express+server listen

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// grab the packages we need
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+// routes will go here
+
+// start the server
+app.listen(port);
+console.log('Server started!');
+
+
 var mailgun = require("mailgun-js");
 var api_key = 'key-a0a6f0f537f651a6afb73234d346a045';
 var DOMAIN = 'sandbox54dd8016480441aaa29353d95fdf3890.mailgun.org';
