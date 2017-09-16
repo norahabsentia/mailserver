71d2e171cf7125107eea17dbcb1402f7d585e636
Ashish Gupta <ashish@absentiavr.com>
2017-09-16 16:46:43 +0530

    Add logic to send mail

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// grab the packages we need
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-var port = process.env.PORT || 8080;
 
-// routes will go here
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// start the server
-app.listen(port);
-console.log('Server started!');
-
+// app.get('/', function(req, res) {
+//     res.sendfile("index.html");
+// });
 app.post('/mail', function(req, res) {
-//     console.log(req)
-    var user_id = req.param('id');
-    var token = req.param('id1');
-    var geo = req.param('id2');
-    console.log(user_id);
-    console.log(token);
+    res.header('Access-Control-Allow-Origin', "*")
+    var mail_from = req.body.name + "<" + req.body.email + ">";
+    var mail_to = 'Ashish Gupta <ashish@absentiavr.com>';
+    var mail_subject = 'A message from norah.ai';
+    var mail_text = "Phone Number: " + req.body.phone + "\n" + req.body.text;
+
+    console.log("Sending Mail");
     var mailgun = require("mailgun-js");
     var api_key = 'key-a0a6f0f537f651a6afb73234d346a045';
     var DOMAIN = 'sandbox54dd8016480441aaa29353d95fdf3890.mailgun.org';
     var mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
 
     var data = {
-        from: user_id,
-        to: token,
-        subject: geo,
-        text: 'Testing some Mailgun awesomness!'
+        from: mail_from,
+        to: mail_to,
+        subject: mail_subject,
+        text: mail_text
     };
 
-    mailgun.messages().send(data, function(error, body) {
-//         console.log(data);
-//         console.log(body);
-    });
-    res.send(user_id + ' ' + token + ' ' + geo);
+    mailgun.messages().send(data, function(error, body) {});
+    res.send("ok");
 });
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Started on PORT 3000");
+})
